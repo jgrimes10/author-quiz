831bf11604c8ff3aca00019622bbb50d8e6330e5
RELIAS\jgrimes <jgrimes@reliaslearning.com>
2018-10-24 09:29:58 -0400

    Write sample tests using Jest. Import enzyme & enzyme for react for more sample tests.

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -2,8 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-    ReactDOM.render(<AuthorQuiz/>, div);
-  ReactDOM.unmountComponentAtNode(div);
+function Hello(props) {
+    return <h1>Hello at {props.now}</h1>
+}
+
+const moment = new Date();
+
+describe('When testing directly', function () {
+    let result;
+    beforeAll(() => {
+        result = Hello({now: moment.toISOString()})
+    });
+
+    it('should return a value', function () {
+        expect(result).not.toBeNull();
+    });
+
+    it('should be an h1', function () {
+        expect(result.type).toBe('h1');
+    });
+
+    it('should have children', function () {
+        expect(result.props.children).toBeTruthy();
+    });
+});
+
+describe('When testing with ReactDOM', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<AuthorQuiz/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
